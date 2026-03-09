c7d82eda6c5730bdfbebcf986cf64b40bc6c8b89
texhnolyze <joegriepen@gmail.com>
2026-03-09 22:51:42 +0100

    feat(subscribers): expect more specific types
    on `hsl_gamecontroller` topics, which are consistent and expected by the
    actual [GameController]:
    - only `(x,y)` position of the ball (`PositionStamped`)
    - custom `(x,y,theta)` pose of the player (custom `PlayerStatusPose`)
    
    In a further step it might also later make sense to just create full
    custom message `PlayerStatus` cotaining all required fields of the
    `RoboCupGameControlReturnData` from the GameController.
    
    [GameController]: https://github.com/RoboCup-HumanoidSoccerLeague/GameController/

--- game_controller_hl/game_controller_hl/receiver.py
+++ game_controller_hl/game_controller_hl/receiver.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import socket
-from typing import Any
 import rclpy
 
 from construct import ConstError
@@ -22,67 +21,72 @@ from rclpy.time import Time
 from rclpy.duration import Duration
 from std_msgs.msg import Header
 from construct import Container
-from geometry_msgs.msg import PoseWithCovarianceStamped, TwistWithCovariance
+from geometry_msgs.msg import PointStamped
 from std_msgs.msg import Float32, Bool
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 from game_controller_hl.gamestate import GameStateStruct, ResponseStruct
 from game_controller_hl.utils import get_parameters_from_other_node
+from game_controller_hl_interfaces.msg import PlayerStatusPose
 
 from game_controller_hl_interfaces.msg import GameState
-from game_controller_hl_interfaces.msg import Ball as BallWithCovariance
 
 
 class GameStateReceiver(Node):
-    """ This class puts up a simple UDP Server which receives the
+    """This class puts up a simple UDP Server which receives the
     *addr* parameter to listen to the packages from the game_controller.
 
     If it receives a package it will be interpreted with the construct data
     structure and the :func:`on_new_gamestate` will be called with the content.
 
-    After this we send a package back to the GC """
+    After this we send a package back to the GC"""
 
     def __init__(self, *args, **kwargs):
-        super().__init__('game_controller', *args, **kwargs, allow_undeclared_parameters=True, automatically_declare_parameters_from_overrides=True)
+        super().__init__(
+            "game_controller",
+            *args,
+            **kwargs,
+            allow_undeclared_parameters=True,
+            automatically_declare_parameters_from_overrides=True,
+        )
 
         # Check if we have the team and bot id parameters or if we should get them from the blackboard
-        if self.has_parameter('team_id') and self.has_parameter('bot_id'):
-            self.get_logger().info('Found team_id and bot_id parameter, using them')
+        if self.has_parameter("team_id") and self.has_parameter("bot_id"):
+            self.get_logger().info("Found team_id and bot_id parameter, using them")
             # Get the parameters from our node
-            self.team_number = self.get_parameter('team_id').value
-            self.player_number = self.get_parameter('bot_id').value
+            self.team_number = self.get_parameter("team_id").value
+            self.player_number = self.get_parameter("bot_id").value
         else:
-            self.get_logger().info('No team_id and bot_id parameter set in game_controller, getting them from blackboard')
+            self.get_logger().info(
+                "No team_id and bot_id parameter set in game_controller, getting them from blackboard"
+            )
             # Get the parameter names from the parameter server
-            param_blackboard_name: str = self.get_parameter('parameter_blackboard_name').value
-            team_id_param_name: str = self.get_parameter('team_id_param_name').value
-            bot_id_param_name: str = self.get_parameter('bot_id_param_name').value
+            param_blackboard_name: str = self.get_parameter("parameter_blackboard_name").value
+            team_id_param_name: str = self.get_parameter("team_id_param_name").value
+            bot_id_param_name: str = self.get_parameter("bot_id_param_name").value
             # Get the parameters from the blackboard
-            params = get_parameters_from_other_node(self, param_blackboard_name, [
-                team_id_param_name,
-                bot_id_param_name])
+            params = get_parameters_from_other_node(
+                self, param_blackboard_name, [team_id_param_name, bot_id_param_name]
+            )
             # Set the parameters
             self.team_number = params[team_id_param_name]
             self.player_number = params[bot_id_param_name]
 
         # Create subscribers
-        self.ball_pose_with_cov: BallWithCovariance
+        self.ball_position: PointStamped
         self.ball_age: Float32
         self.is_fallen: Bool
-        self.pose: PoseWithCovarianceStamped
-        self.create_subscription(BallWithCovariance, "hsl_gamecontroller/ball_with_covariance", self.ball_pose_with_cov_cb, 1)
+        self.pose: PlayerStatusPose
+        self.create_subscription(PointStamped, "hsl_gamecontroller/ball_position", self.ball_position_cb, 1)
         self.create_subscription(Float32, "hsl_gamecontroller/ball_age", self.ball_age_cb, 1)
         self.create_subscription(Bool, "hsl_gamecontroller/is_fallen", self.is_fallen_cb, 1)
-        self.create_subscription(PoseWithCovarianceStamped, "hsl_gamecontroller/pose_stamped", self.pose_cb, 1)
-        
+        self.create_subscription(PlayerStatusPose, "hsl_gamecontroller/pose", self.pose_cb, 1)
 
-        self.get_logger().info(f'We are playing as player {self.player_number} in team {self.team_number}')
-
-        self.get_logger().info(f'We are playing as player {self.player_number} in team {self.team_number}')
+        self.get_logger().info(f"We are playing as player {self.player_number} in team {self.team_number}")
 
         # The publisher for the game state
-        self.state_publisher = self.create_publisher(GameState, 'gamestate', 1)
+        self.state_publisher = self.create_publisher(GameState, "gamestate", 1)
 
-        #The publisher for the diagnostics
+        # The publisher for the diagnostics
         self.diagnostic_pub = self.create_publisher(DiagnosticArray, "diagnostics", 1)
 
         # The time in seconds after which we assume the game controller is lost
@@ -90,11 +94,8 @@ class GameStateReceiver(Node):
         self.game_controller_lost_time = 5
 
         # The address listening on and the port for sending back the robots meta data
-        self.addr = (
-            self.get_parameter('listen_host').value,
-            self.get_parameter('listen_port').value
-        )
-        self.answer_port = self.get_parameter('answer_port').value
+        self.addr = (self.get_parameter("listen_host").value, self.get_parameter("listen_port").value)
+        self.answer_port = self.get_parameter("answer_port").value
 
         # The time of the last package
         self.last_package_time: Time = self.get_clock().now()
@@ -102,20 +103,20 @@ class GameStateReceiver(Node):
         # Create the socket we want to use for the communications
         self.socket = self._open_socket()
 
-    def ball_pose_with_cov_cb(self, msg: PoseWithCovarianceStamped):
-        self.ball_pose_with_cov = msg
-    
+    def ball_position_cb(self, msg: PointStamped):
+        self.ball_position = msg
+
     def ball_age_cb(self, msg: Float32):
         self.ball_age = msg
-    
+
     def is_fallen_cb(self, msg: Bool):
         self.is_fallen = msg
-    
-    def pose_cb(self, msg: PoseWithCovarianceStamped):
+
+    def pose_cb(self, msg: PlayerStatusPose):
         self.pose = msg
 
     def _open_socket(self) -> socket.socket:
-        """ Creates the socket """
+        """Creates the socket"""
         new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         new_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         new_socket.bind(self.addr)
@@ -123,17 +124,18 @@ class GameStateReceiver(Node):
         return new_socket
 
     def receive_forever(self):
-        """ Waits in a loop for new packages """
+        """Waits in a loop for new packages"""
         while rclpy.ok():
             # Try to receive a package
             self.receive_and_answer_once()
             # Check if we didn't receive a package for a long time for publishing diagnostics
-            received_message_lately = self.get_time_since_last_package() < Duration(seconds=self.game_controller_lost_time)
+            received_message_lately = self.get_time_since_last_package() < Duration(
+                seconds=self.game_controller_lost_time
+            )
             self.publish_diagnostics(received_message_lately)
 
-
     def receive_and_answer_once(self):
-        """ Receives a package, interprets it and sends an answer. """
+        """Receives a package, interprets it and sends an answer."""
         try:
             # Receive the package
             data, peer = self.socket.recvfrom(GameStateStruct.sizeof())
@@ -158,7 +160,7 @@ class GameStateReceiver(Node):
         except ConstError:
             self.get_logger().warn("Parse Error: Probably using an old protocol!")
         except IOError as e:
-                self.get_logger().warn(f"Error while sending keep-alive: {str(e)}")
+            self.get_logger().warn(f"Error while sending keep-alive: {str(e)}")
 
     def publish_diagnostics(self, received_message_lately: bool):
         """
@@ -168,10 +170,14 @@ class GameStateReceiver(Node):
         diag_array = DiagnosticArray()
 
         # configure DiagnosticStatus message
-        diag = DiagnosticStatus(name = "Game Controller", hardware_id = "Game Controller")
+        diag = DiagnosticStatus(name="Game Controller", hardware_id="Game Controller")
         if not received_message_lately:
             self.get_logger().info("No GameController message received", throttle_duration_sec=5)
-            diag.message = "Lost connection to game controller for " + str(int(self.get_time_since_last_package().nanoseconds / 1e9)) + " sec"
+            diag.message = (
+                "Lost connection to game controller for "
+                + str(int(self.get_time_since_last_package().nanoseconds / 1e9))
+                + " sec"
+            )
             diag.level = DiagnosticStatus.WARN
         else:
             diag.message = "Connected"
@@ -183,83 +189,81 @@ class GameStateReceiver(Node):
         diag_array.header.stamp = self.get_clock().now().to_msg()
         self.diagnostic_pub.publish(diag_array)
 
-
     def answer_to_gamecontroller(self, peer):
-        """ Sends a life sign to the game controller """
+        """Sends a life sign to the game controller"""
         # Build the answer package
-        data = ResponseStruct.build(dict(
-            player_number = self.player_number,
-            team_number = self.team_number,
-            fallen = self.is_fallen,
-            pose = [self.pose.pose.position.x, self.pose.pose.position.y, self.pose.pose.position.z],
-            ball_age= self.ball_age,
-            ball = [self.ball_pose_with_cov.point.x,self.ball_pose_with_cov.point.y]))
+        data = ResponseStruct.build(
+            dict(
+                player_number=self.player_number,
+                team_number=self.team_number,
+                fallen=self.is_fallen,
+                pose=self.pose,
+                ball=[self.ball_position.point.x, self.ball_position.point.y],
+                ball_age=self.ball_age,
+            )
+        )
         # Send the package
-        self.get_logger().debug(f'Sending answer to {peer[0]}:{self.answer_port}')
+        self.get_logger().debug(f"Sending answer to {peer[0]}:{self.answer_port}")
         try:
             self.socket.sendto(data, (peer[0], self.answer_port))
         except Exception as e:
             self.get_logger().error(f"Network Error: {str(e)}")
 
     def build_game_state_msg(self, state) -> GameState:
-        """ Builds a GameState message from the game state """
+        """Builds a GameState message from the game state"""
 
         # Get the team objects sorted into own and rival team
-        own_team = GameStateReceiver.select_team_by(
-            lambda team: team.team_number == self.team_number,
-            state.teams)
-        rival_team = GameStateReceiver.select_team_by(
-            lambda team: team.team_number != self.team_number,
-            state.teams)
+        own_team = GameStateReceiver.select_team_by(lambda team: team.team_number == self.team_number, state.teams)
+        rival_team = GameStateReceiver.select_team_by(lambda team: team.team_number != self.team_number, state.teams)
 
         # Add some assertions to make sure everything is fine
-        assert not (own_team is None or rival_team is None), \
-            f'Team {self.team_number} not playing, only {state.teams[0].team_number} and {state.teams[1].team_number}'
+        assert not (own_team is None or rival_team is None), (
+            f"Team {self.team_number} not playing, only {state.teams[0].team_number} and {state.teams[1].team_number}"
+        )
 
-        assert self.player_number <= len(own_team.players), \
-            f'Robot {self.player_number} not playing'
+        assert self.player_number <= len(own_team.players), f"Robot {self.player_number} not playing"
 
         this_robot = own_team.players[self.player_number - 1]
 
         return GameState(
-            header = Header(stamp=self.get_clock().now().to_msg()),
-            players_per_team = state.players_per_team,
-            competition_phase = state.competition_phase.intvalue,
-            game_phase = state.game_phase.intvalue,
-            main_state = state.state.intvalue,
-            set_play = state.set_play.intvalue,
-            kicking_team = state.kicking_team,
-            first_half = state.first_half,
-            own_score = own_team.score,
-            rival_score = rival_team.score,
-            secs_remaining = state.secs_remaining,
-            secondary_time = state.secondary_time,
-            penalized = this_robot.penalty != 0,
-            seconds_till_unpenalized = this_robot.secs_till_unpenalized,
-            own_player_color = own_team.field_player_color.intvalue,
-            own_goalie_color = own_team.goalkeeper_color.intvalue,
-            rival_player_color = rival_team.field_player_color.intvalue,
-            rival_goalie_color = rival_team.goalkeeper_color.intvalue,
-            #--- Gibt es nicht mehr? ---
-            #drop_in_team = state.drop_in_team,
-            #drop_in_time = state.drop_in_time,
-            penalty_shot = own_team.penalty_shot,
-            single_shots = own_team.single_shots,
-            #--- Gibt es nicht mehr? Waren die ähnlich wie Message Budget? ---
-            #coach_message = own_team.coach_message,
-            message_budget = own_team.message_budget,
-            team_mates_with_penalty = [player.penalty != 0 for player in own_team.players],
-            #--- Gibt es nicht mehr? Selber bemerken ob man rot hat? ---
-            #team_mates_with_red_card = [player.number_of_red_cards != 0 for player in own_team.players],
+            header=Header(stamp=self.get_clock().now().to_msg()),
+            players_per_team=state.players_per_team,
+            competition_phase=state.competition_phase.intvalue,
+            game_phase=state.game_phase.intvalue,
+            main_state=state.state.intvalue,
+            set_play=state.set_play.intvalue,
+            kicking_team=state.kicking_team,
+            first_half=state.first_half,
+            own_score=own_team.score,
+            rival_score=rival_team.score,
+            secs_remaining=state.secs_remaining,
+            secondary_time=state.secondary_time,
+            penalized=this_robot.penalty != 0,
+            seconds_till_unpenalized=this_robot.secs_till_unpenalized,
+            own_player_color=own_team.field_player_color.intvalue,
+            own_goalie_color=own_team.goalkeeper_color.intvalue,
+            rival_player_color=rival_team.field_player_color.intvalue,
+            rival_goalie_color=rival_team.goalkeeper_color.intvalue,
+            # --- Gibt es nicht mehr? ---
+            # drop_in_team = state.drop_in_team,
+            # drop_in_time = state.drop_in_time,
+            penalty_shot=own_team.penalty_shot,
+            single_shots=own_team.single_shots,
+            # --- Gibt es nicht mehr? Waren die ähnlich wie Message Budget? ---
+            # coach_message = own_team.coach_message,
+            message_budget=own_team.message_budget,
+            team_mates_with_penalty=[player.penalty != 0 for player in own_team.players],
+            # --- Gibt es nicht mehr? Selber bemerken ob man rot hat? ---
+            # team_mates_with_red_card = [player.number_of_red_cards != 0 for player in own_team.players],
         )
 
     def get_time_since_last_package(self) -> Duration:
-        """ Returns the time in seconds since the last package was received """
+        """Returns the time in seconds since the last package was received"""
         return self.get_clock().now() - self.last_package_time
 
     @staticmethod
     def select_team_by(predicate: callable, teams: list[Container]) -> Container:
-        """ Selects the team based on the predicate """
+        """Selects the team based on the predicate"""
         selected = [team for team in teams if predicate(team)]
         return next(iter(selected), None)
 
@@ -275,5 +279,5 @@ def main(args=None):
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
